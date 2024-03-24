eebe9c4af7f4eab6e1ebd156d0451abc984c8553
daniyalaamir110 <daniyal.amir110@gmail.com>
2024-03-25 03:17:30 +0500

    Class based views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+
     # path('<int:id>/', views.post_detail, name='post_detail'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    # path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def post_list(request):
@@ -24,6 +25,31 @@ def post_list(request):
         context={'posts': posts}
     )
 
+
+class PostListView(ListView):
+    """
+    Class-based view for listing posts.
+    Alternative to the function-based view post_list.
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+# def post_detail(request, id):
+#     post = get_object_or_404(
+#         Post,
+#         id=id,
+#         status=Post.Status.PUBLISHED
+#     )
+#     return render(
+#         request=request, 
+#         template_name='blog/post/detail.html', 
+#         context={'post': post}
+#     )
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post, 
@@ -38,3 +64,20 @@ def post_detail(request, year, month, day, post):
         template_name='blog/post/detail.html', 
         context={'post': post}
     )
+
+
+class PostDetailView(DetailView):
+    """
+    Class-based view for viewing a single post.
+    Alternative to the function-based view post_detail.
+    """
+
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/post/detail.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'post'
+    query_pk_and_slug = True
+    # pk_url_kwarg = 'id'
+    # pk_url_kwarg = 'pk'
+    # pk_url_kwarg = 'post
