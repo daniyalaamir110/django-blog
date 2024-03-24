acf08339812018904570996386e1c5fa5b93a9b0
daniyalaamir110 <daniyal.amir110@gmail.com>
2024-03-25 03:01:13 +0500

    Pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list(request):
-    posts = get_list_or_404(Post, status=Post.Status.PUBLISHED)
+    post_list = Post.published.all()
+    
+    paginator = Paginator(post_list, 3)
+    page = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
+    # Shorthand version
+    # posts = paginator.get_page(page)
+
     return render(
         request=request, 
         template_name='blog/post/list.html', 
         context={'posts': posts}
     )
-
-
-# def post_detail(request, id):
-#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-#     return render(
-#         request=request, 
-#         template_name='blog/post/detail.html', 
-#         context={'post': post}
-#     )
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
